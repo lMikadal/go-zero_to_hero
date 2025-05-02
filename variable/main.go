8c4d3d797ccc0d2751b8567f0307de1d7e6f6571
Panupong Mikada <panupong.mikada@gmail.com>
2025-05-02 23:08:52 +0700

    update zero value

--- variable/main.go
+++ variable/main.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-// global case 1 (zero value)
+// global case 1
 var global bool
 
 // global case 2
@@ -19,10 +19,16 @@ func main() {
 	// local case 3 (short declaration)
 	z := 3
 
+	// zero value
+	var i int
+	var s string
+	var b bool
+
 	// not use
 	_ = global
 	_ = global_2
 
 	fmt.Printf("x: %v, y: %v, z: %v\n", x, y, z)
-	fmt.Printf("global 1: %v, 2: %v\n", global, global_2)
+	fmt.Printf("global => 1: %v, 2: %v\n", global, global_2)
+	fmt.Printf("zero value => int: %#v, string: %#v, bool: %#v\n", i, s, b)
 }
